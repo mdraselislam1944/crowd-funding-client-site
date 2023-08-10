a0d1f81ada4d2808c45df7786e5e7ed755401e72
Sakib-Atreus <sakibatreus@gmail.com>
2023-08-11 02:31:29 +0600

    routes

---
+++ src/components/Home/Campaign/Campaign.jsx
@@ -0,0 +1,18 @@
+const Campaign = () => {
+    return (
+        <div>
+            <h1 className="text-center text-2xl font-bold">Feature Campaigns</h1>
+            <hr className="w-20 border-2 rounded border-orange-400" />
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Campaign;
