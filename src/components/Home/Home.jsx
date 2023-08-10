9204089709a24fb33573702190d4ad90cbf850e6
Sakib-Atreus <sakibatreus@gmail.com>
2023-08-11 02:55:29 +0600

    setup done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Campaign from "./Campaign/Campaign";
+import Success from "./Success/Success";
 
 const Home = () => {
     return (
         <div>
             <Campaign></Campaign>
+            <Success></Success>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import router from './components/Routes/Routes';
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Main from './layout/Main.jsx';
-import Home from './components/Home/Home';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:  <Main></Main>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>
-      }
-    ]
-  },
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <RouterProvider router={router} />
-  
   </React.StrictMode>,
 )
