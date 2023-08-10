be62ba8af38991b384ab5261c34016bdd8d8066f
Sakib-Atreus <sakibatreus@gmail.com>
2023-08-10 15:10:02 +0600

    atreus

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ function App() {
         </p>
 
         <h1 className='bg-blue-400 font-extrabold p-4 text-red-600'>Sakib Boss!</h1>
+
+        <h1 className='bg-blue-400 font-extrabold p-4 text-red-600'>Sakib Atreus!</h1>
+
+        <h1 className='bg-blue-400 font-extrabold p-4 text-red-600'>Sakib Boss!</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
