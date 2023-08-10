9cc831d11bc953bc3287767953da7a748dafabea
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-11 03:46:48 +0600

    LLL

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import router from './components/Routes/Routes';
+import router from './Components/Routes/Routes';
 
 import {
   RouterProvider,
