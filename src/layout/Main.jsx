37f9a6aa721a9422f9df96922852f9949d00f6fe
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-11 02:12:40 +0600

    l

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Success from "../components/Home/Success/Success";
 
 
 const Main = () => {
     return (
         <div>
-            
+            <Success></Success>
             <Outlet></Outlet>
           
         </div>
