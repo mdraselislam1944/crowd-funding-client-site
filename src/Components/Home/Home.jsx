cd2d9f5f988073a38a4954f4277b8fce4fe10207
mahdimortuza <mahdi.webx@gmail.com>
2023-08-11 11:39:17 +0600

    footer outside

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,7 @@
 import About from "./About/About";
 import Campaign from "./Campaign/Campaign";
 import Category from "./Category/Category";
-import Footer from "./Footer/Footer";
 import News from "./News/News";
-import Partners from "./Partners/Partners";
 import Success from "./Success/Success";
 
 const Home = () => {
@@ -14,8 +12,6 @@ const Home = () => {
             <Category></Category>
             <Success></Success>
             <News></News>
-            <Partners></Partners>
-            <Footer></Footer>
         </div>
     );
 };
