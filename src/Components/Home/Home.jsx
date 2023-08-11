0ed7b394d5586d1cf235cc0994ae5ac76048f8ff
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-11 06:36:35 +0600

    added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,15 @@
+import About from "./About/About";
 import Campaign from "./Campaign/Campaign";
+import Category from "./Category/Category";
 import News from "./News/News";
 import Success from "./Success/Success";
 
 const Home = () => {
     return (
         <div>
+            <About></About>
             <Campaign></Campaign>
+            <Category></Category>
             <Success></Success>
             <News></News>
         </div>
