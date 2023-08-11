f2fe9e68f60627b67482355a6e62a959011233a4
Shahadat171 <shahadathosan999@gmail.com>
2023-08-11 12:07:40 +0600

    header fro shahadat

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,12 +2,16 @@ import Animation from "../Animation/Animation";
 import About from "./About/About";
 import Campaign from "./Campaign/Campaign";
 import Category from "./Category/Category";
+import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
 import News from "./News/News";
 import Success from "./Success/Success";
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
+            <Header></Header>
             <About></About>
             <Campaign></Campaign>
             <Category></Category>
