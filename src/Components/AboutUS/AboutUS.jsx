671ee44d5a3282a18a20ac43ddc7367b8bd5eb29
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-17 03:14:47 +0600

    o

--- src/Components/AboutUS/AboutUS.jsx
+++ src/Components/AboutUS/AboutUS.jsx
@@ -1,11 +1,7 @@
-import Partners from "../Home/Partners/Partners";
-
 const AboutUs = () => {
     return (
         <div>
-           
-            <h1>snfhsfhdbshcfdhc</h1>
-            <Partners></Partners>
+
         </div>
     );
 };
