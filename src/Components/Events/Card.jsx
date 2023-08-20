dde484ae1cf563aacbae27358396edea593fb626
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-20 21:29:15 +0600

    card update

--- src/Components/Events/Card.jsx
+++ src/Components/Events/Card.jsx
@@ -1,26 +1,43 @@
 import { Link } from "react-router-dom";
-
+import { AiOutlineFall } from 'react-icons/ai';
+//[#F99F24]
 const Card = ({ item }) => {
     const { image,
         title,
         description,
         publish_date,
-        publisher_name } = item;
+        publisher_name,
+        publisher_image } = item;
     return (
-        <div className="card w-96 mb-5 bg-gray-200 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
-            <div className="card-body">
+        <div className="card w-96 mb-5 text-white  shadow-none ">
+           
+           <div className="">
+           <figure><img className="w-100% h-64 rounded-bl-full " src={image} alt="" /></figure>
+          
+           <AiOutlineFall className="absolute -mt-16 w-16 bg-transparent bg-rounded-full  text-[#F99F24] h-16"></AiOutlineFall>
+           
+           </div>
+            <div className="card-body ">
                 <h2 className="card-title">
                     {title}
                 </h2>
-                <p>{description}</p>
+                <p className="p-2">{description}</p>
+                <hr className="h-px bg-[#F99F24] border-0 dark:bg-gray-700"/>
+
                 <div className="card-actions justify-between">
-                    <div className="">{publish_date}
+                    <div className="flex gap-3">
+                        <div>
+                            <img className="rounded-full w-12 h-12" src={publisher_image} alt="" />
+                        </div>
+                       <div>
+                       {publish_date}
                         <br />
                         {publisher_name}
+                       </div>
                     </div>
+                    
                     <Link to="/about">
-                        <button className="badge bg-[#F99F24] p-2 text white">Read More</button>
+                        <button className="btn-xs rounded-full bg-[#F99F24]  text-black">Read More</button>
                     </Link>
                 </div>
 
--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import img from "../../assets/img/section-banner/larm-rmah-AEaTUnvneik-unsplash.jpg"
 import SharedBanner from "../Contact/SharedBanner";
+import bg from "../../assets/Video/wallpaperflare.com_wallpaper.jpg"
 import Card from "./Card";
 const Events = () => {
     const [items, setItems]= useState([]);
@@ -12,9 +13,9 @@ const Events = () => {
            setItems(data);
        })
     }, [])
-
+//bg-gradient-to-r from-neutral-600 from-10% via- via-50% to-neutral-600 to-90%
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="bg-cover bg-center bg-no-repeat  max-w-7xl mx-auto " style={{ backgroundImage: `url(${bg})` }}>
             <div>
                 <SharedBanner
                     background={img}
@@ -29,11 +30,11 @@ const Events = () => {
                     }
                 ></SharedBanner>
                 <div className="text-center mt-8">
-                    <h2 className="text-3xl font-bold">Our All Events</h2>
-                    <hr className="border-b-[3px] w-[106px] mt-1 mb-2 border-[#F99F24] mx-auto" />
+                    <h2 className="text-3xl font-bold text-white ">Our All Events</h2>
+                    <hr className="border-b-[3px] w-[106px] mt-1 mb-5 border-[#F99F24] mx-auto" />
                 </div>
             </div>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 m-2 gap-2" >
+            <div className= " grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2  gap-2" >
               {
                 items?.map(item=> <Card
                     key={item._id}
