70e20e8b53cb8468299dd5dad5d30d6a86e194f7
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-21 00:47:26 +0600

    L

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BsSearch } from "react-icons/bs";
 import img from "../../assets/img/section-banner/larm-rmah-AEaTUnvneik-unsplash.jpg"
 import SharedBanner from "../Contact/SharedBanner";
 import bg from "../../assets/Video/wallpaperflare.com_wallpaper.jpg"
@@ -33,6 +34,17 @@ const Events = () => {
                     <h2 className="text-3xl font-bold text-white ">Our All Events</h2>
                     <hr className="border-b-[3px] w-[106px] mt-1 mb-5 border-[#F99F24] mx-auto" />
                 </div>
+                 <div className="mb-6 relative w-10/12 mx-auto md:w-[20%]  ">
+                <input
+                    type="text"
+                    placeholder="Search Here..."
+                    value=""
+                    onChange=""
+                    className="w-full px-4 py-2 mt-3  border border-black rounded-full text-black placeholder-black
+                    bg-gradient-to-r from-[#F99F24] from-10% to-white to-90%"
+                />
+                <BsSearch className="absolute top-[30%] right-2 text-black  p-1 w-8 h-8 rounded-full" />
+            </div>
             </div>
             <div className= " grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2  gap-2" >
               {
