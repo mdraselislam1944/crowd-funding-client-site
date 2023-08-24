8fae71dcebfca1f5ce5d4c6c272742ab022aa95b
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-25 01:57:01 +0600

    Nav

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="bg-black md:bg-opacity-50 bg-opacity-90 px-7 md:px-0 text-white p-4 shadow font-poppins w-full fixed z-10">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="flex items-center">
+        <div className="flex items-center ms-3">
           <Link to="/">
             <img src={logo} alt="" className="w-36" />
           </Link>
@@ -65,10 +65,23 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+       
         <div className="hidden md:flex items-center">
-          <Link to="login" className="btn btn-outline border-yellow-600 text-white hover:text-yellow-600 rounded transition duration-300 hover:bg-inherit hover:border-yellow-600">
-            Login
-          </Link>
+        {
+            user ?
+              <Link><button className='btn bg-[#F99F24] text-white border-none me-3 px-6 hover:bg-black hover:text-[#F99F24]' onClick="">LogOut</button></Link>
+              :
+              <Link to="/login">
+                <button
+                  type="button" onClick={handleClose}
+                  className="btn bg-[#F99F24] text-white border-none me-3 px-6 hover:bg-black hover:text-[#F99F24]"
+                >
+                  Login
+                </button>
+              </Link>
+          }
+          {/*   className="text-white border border-yellow-600 bg-inherit hover:bg-[#F99F24] focus:ring-4 focus:outline-none focus:ring-[#F99F24] font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 mt-6" */}
+          
         </div>
         <div className="md:hidden">
           <Link onClick={toggleMenu} className="text-white focus:outline-none">
@@ -143,12 +156,12 @@ const Navbar = () => {
           </ul>
           {
             user ?
-              <Link><button className='mx-10 btn btn-success' onClick="">logOut</button></Link>
+              <Link><button className="text-[#F99F24] border border-[#F99F24] bg-inherit hover:bg-[#F99F24] focus:ring-4 focus:outline-none focus:ring-[#F99F24] font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 mt-6" onClick="">LogOut</button></Link>
               :
               <Link to="/login">
                 <button
                   type="button" onClick={handleClose}
-                  className="text-white border border-yellow-600 bg-inherit hover:bg-[#F99F24] focus:ring-4 focus:outline-none focus:ring-[#F99F24] font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 mt-6"
+                  className="text-[#F99F24] border border-[#F99F24] bg-inherit hover:bg-[#F99F24] focus:ring-4 focus:outline-none focus:ring-[#F99F24] font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 mt-6"
                 >
                   Login
                 </button>
